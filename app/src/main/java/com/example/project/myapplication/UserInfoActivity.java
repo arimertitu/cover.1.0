0a4018c4ebcf54eaebbfa652bd5d171f7e14e649
svsosm <savasosmnglu@gmail.com>
2018-09-26 00:47:06 +0300

    database 2
    add database register page
    add database userinfo page

--- app/src/main/java/com/example/project/myapplication/UserInfoActivity.java
+++ app/src/main/java/com/example/project/myapplication/UserInfoActivity.java
@@ -1,6 +1,8 @@
 package com.example.project.myapplication;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,8 +11,15 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.util.Calendar;
@@ -21,7 +30,10 @@ public class UserInfoActivity extends AppCompatActivity {
     private Button btnSubmit;
     private ImageButton btnSelectDate;
     private TextView txtBirthday;
-    private RadioButton genderMale,genderFemale;
+    private RadioGroup genderGroup;
+    private String name,surname,birthday,gender;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +45,13 @@ public class UserInfoActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("User Information");
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         setupUIViews();
+
+
+
 
 
         btnSelectDate.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +79,45 @@ public class UserInfoActivity extends AppCompatActivity {
         });
 
 
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int secimgender = genderGroup.getCheckedRadioButtonId();
+
+                switch (secimgender){
+                    case R.id.gender_male:
+                        gender = "M";
+                        break;
+                    case R.id.gender_female:
+                        gender = "F";
+                        break;
+                }
+
+                name = edtName.getText().toString();
+                surname = edtSurname.getText().toString();
+                birthday = txtBirthday.getText().toString();
+
+                String user_id =firebaseAuth.getCurrentUser().getUid();
+                databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+                databaseReference.child("name").setValue(name);
+                databaseReference.child("surname").setValue(surname);
+                databaseReference.child("gender").setValue(gender);
+                databaseReference.child("birthday").setValue(birthday).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(UserInfoActivity.this,MainActivity.class));
+                        }else {
+                            Toast.makeText(UserInfoActivity.this,"Server is busy ",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
 
     }
 
@@ -72,8 +128,8 @@ public class UserInfoActivity extends AppCompatActivity {
         btnSelectDate = (ImageButton) findViewById(R.id.btnSelectDate);
         btnSubmit = (Button)findViewById(R.id.btnSubmit);
         txtBirthday = (TextView) findViewById(R.id.txtbirthday);
-        genderMale = (RadioButton) findViewById(R.id.gender_male);
-        genderFemale = (RadioButton) findViewById(R.id.gender_female);
+        genderGroup = (RadioGroup) findViewById(R.id.gender);
+
 
 
 
